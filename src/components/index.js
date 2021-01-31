36af1768880ce7f08b8d78bb1c6f7665882538b1
antoniovictormt <antoniovictormt@hotmail.com>
2021-01-31 15:43:13 -0300

    first commit from Imersão React Alura

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as QuizBackground } from './QuizBackground';
+export { default as QuizCorner } from './QuizCorner';
+export { default as QuizFooter } from './QuizFooter';
+export { default as QuizLogo } from './QuizLogo';
+export { default as QuizWidget } from './QuizWidget';
